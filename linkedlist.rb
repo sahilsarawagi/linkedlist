7e60457d3ca35281f31b7586d1bdea974e79cd50
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-02-29 21:29:51 +0530

    Implement basic linkedlist

---
+++ linkedlist.rb
@@ -0,0 +1,146 @@
+class Node
+  def initialize 
+    @value = nil
+    @node = nil
+  end
+  def value=(value)
+    @value = value
+  end
+  def get_value
+    @value
+  end
+  def next_node=(node)
+    @node = node
+  end
+  def get_next_node
+    @node
+  end
+end
+
+class LinkedList
+
+  def initialize
+    @head = Node.new
+    @tail = @head
+  end
+
+  def append(value)
+    if @head.get_value==nil
+      @head.value= value
+    else
+      node = Node.new
+      node.value= value
+      @tail.next_node=node
+      @tail = node
+    end
+  end
+
+  def prepend(value)
+    if @head.get_value==nil
+      append(value)
+    else
+      node = Node.new
+      node.value= value
+      node.next_node= @head
+      @head = node
+    end
+  end
+
+  def size
+    count =0
+    node = @head
+    while node!=nil
+      node=node.get_next_node
+      count+=1
+    end
+   count  
+  end
+
+  def head
+    @head
+  end
+
+  def tail
+    @tail
+  end
+
+  def at(index)
+    node = @head
+    while index>0
+      node = node.get_next_node
+      index-=1
+    end
+    node
+  end
+
+  def pop
+    if @head.get_value==nil
+      puts "List has no element to delete"
+    elsif @head == @tail
+      @head.value =nil
+    else
+      node = @head
+      while node.get_next_node!=@tail
+        node = node.get_next_node
+      end
+      @tail = node
+    end
+  end
+
+  def contains?(value)
+    node =@head
+    while node!= nil
+      if node.get_value==value
+        return true
+      end
+      node= node.get_next_node
+    end
+    false
+  end
+
+  def find(value)
+    index =0 
+    node =@head
+    while node!= nil
+      if node.get_value==value
+        return index
+      end
+      index+=1
+      node= node.get_next_node
+    end
+    nil
+  end
+
+  def to_s
+    return 'nil' if @head.nil?
+    result=""
+    node = @head
+    while node!=nil
+      result += "#{node.get_value}->"
+      node =node.get_next_node
+    end
+    result + "nil"
+  end
+end
+
+
+li = LinkedList.new
+li.append(5)
+li.append(6)
+li.append(5)
+li.append(6)
+p li.to_s
+
+li.prepend(2)
+
+p li.to_s
+# li.pop
+
+
+p li.at(1).get_value
+p li.find(5)
+p li.size
+p li.head.get_value
+p li.tail.get_value
+p li.contains?(7)
+p li.contains?(5)
